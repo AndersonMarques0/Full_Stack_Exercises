dd0faa91792c0969abea9a058d67027e2d70f67d
Anderson da Silva Marques <anderson.silvamarques00@gmail.com>
2025-06-21 15:38:01 -0300

    course-information step 5

--- Part-1/course-information/src/components/content.jsx
+++ Part-1/course-information/src/components/content.jsx
@@ -4,9 +4,9 @@ const Content = (props) => {
 
     return (
         <>
-           <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-           <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-           <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+           <Part part={props.course.parts[0].name} exercise={props.course.parts[0].exercises} />
+           <Part part={props.course.parts[1].name} exercise={props.course.parts[1].exercises} />
+           <Part part={props.course.parts[2].name} exercise={props.course.parts[2].exercises} />
         </>
     )
 }
