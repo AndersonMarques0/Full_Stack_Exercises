cce954f861df26f8934bc7b8ca5a928b98bb9737
Anderson da Silva Marques <anderson.silvamarques00@gmail.com>
2026-03-26 17:54:41 -0300

    phonebook database step 8

--- part2/phonebook/src/components/notification.jsx
+++ part2/phonebook/src/components/notification.jsx
@@ -1,30 +1,13 @@
-
-const Notification = (props) => {
-    console.log('Hello from Notification, props', props)
-    const {message, css} = props
-    console.log('Hello from Notification, message: ', message)
-    console.log('Hello from Notification, css: ', css)
-
-    if(message === null) {
+const Notification = ({ message, css }) => {
+    if (!message) {
         return null
-    } else {
-        if(css === 'positive') {
-            return (
-                <div className="positive">
-                    {message}
-                </div>
-            )
-        } else if(css === 'negative') {
-            return (
-                <div className="negative">
-                    {message}
-                </div>
-            )
-        }
     }
 
-    
-
+    return (
+        <div className={css}>
+            {message}
+        </div>
+    )
 }
 
 export default Notification
--- part2/phonebook/src/components/personForm.jsx
+++ part2/phonebook/src/components/personForm.jsx
@@ -2,76 +2,68 @@ import servicePersons from '../services/servicePersons'
 
 const PersonForm = (props) => {
 
-    const addPerson = event => {
-      event.preventDefault()
-      let changeNumber
-      let id
-      for (const person of props.persons) {
-        
-        if(person.number !== props.valueNumber && person.name === props.valueName) {
-          changeNumber = confirm(`${person.name} is already added to phonebook, replace the old number with a new one?`)
-          id = person.id
-          if(!changeNumber) {
-            return
-          }
-        } else if(person.number === props.valueNumber) {
-          alert(`${person.name} is already added to phonebook`)
-          return
-        }
+  const addPerson = event => {
+    event.preventDefault()
+    const existingPerson = props.persons.find(p => p.name === props.valueName)
 
-         
-        
-      }
-
-      if(changeNumber) {
-
-        const pe = props.persons.find(p => p.id == id)
-        const changedPerson = { ...pe, number: props.valueNumber}
-        servicePersons.update(id, changedPerson)
-        .then(response => {
-          props.setPersons(props.persons.map(person => person.id === id ? response.data : person))
-        })
-        props.messageType('positive')
-        props.handleMessage(`${changedPerson.name} changed your number!`)
-        setTimeout(() => {
-          props.handleMessage(null)
-        }, 5000)
+    if (existingPerson) {
+      if (existingPerson.number === props.valueNumber) {
+        alert(`${props.valueName} is already added to phonebook with this number`)
         return
       }
 
-      const newPerson = {
-        name: props.valueName,
-        number: props.valueNumber,
-        id: String(props.persons.length + 1)
-      }
+      const confirmUpdate = window.confirm(
+        `${props.valueName} is already added to phonebook, replace the old number with a new one?`
+      )
 
-      servicePersons
+      if (confirmUpdate) {
+        const changedPerson = { ...existingPerson, number: props.valueNumber }
+        
+        servicePersons
+          .update(existingPerson.id, changedPerson)
+          .then(response => {
+            props.setPersons(props.persons.map(p => p.id !== existingPerson.id ? p : response.data))
+            props.messageType('positive')
+            props.handleMessage(`Updated ${changedPerson.name}'s number`)
+            setTimeout(() => props.handleMessage(null), 5000)
+          })
+          .catch(error => {
+            props.messageType('negative')
+            props.handleMessage(error.response.data.error)
+            setTimeout(() => props.handleMessage(null), 5000)
+          })
+      }
+      return
+    }
+
+    const newPerson = {
+      name: props.valueName,
+      number: props.valueNumber,
+    }
+
+    servicePersons
       .create(newPerson)
       .then(response => {
         props.setPersons(props.persons.concat(response.data))
+        props.messageType('positive')
+        props.handleMessage(`Added ${newPerson.name}`)
+        setTimeout(() => props.handleMessage(null), 5000)
       })
-
-      props.messageType('positive')
-      props.handleMessage(`${newPerson.name} added!`)
-      setTimeout(() => {
-        props.handleMessage(null)
-      }, 5000)
-
+      .catch(error => {
+        props.messageType('negative')
+        props.handleMessage(error.response.data.error)
+        console.log(error.response.data.error)
+        setTimeout(() => props.handleMessage(null), 5000)
+      })
   }
 
-    return (
-        <form onSubmit={addPerson}>
-            <div>
-            name: <input value={props.valueName} onChange={props.handleName} />
-            </div>
-            <div>
-            number: <input value={props.valueNumber} onChange={props.handleNumber} />
-            </div>
-            <div>
-            <button type="submit" >add</button>
-            </div>
-        </form>
-    )
+  return (
+    <form onSubmit={addPerson}>
+      <div>name: <input value={props.valueName} onChange={props.handleName} /></div>
+      <div>number: <input value={props.valueNumber} onChange={props.handleNumber} /></div>
+      <div><button type="submit">add</button></div>
+    </form>
+  )
 }
 
 export default PersonForm
