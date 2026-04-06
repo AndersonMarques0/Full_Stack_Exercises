cee57f0749c3b17698f1250da578e936ee20900a
Anderson da Silva Marques <anderson.silvamarques00@gmail.com>
2026-04-06 14:47:55 -0300

    unit tests step 2

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -5,6 +5,13 @@ const dummy = (blogs) => {
     
 }
 
+const totalLikes = (blogs) => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
 export default {
-    dummy
+    dummy,
+    totalLikes
 }
