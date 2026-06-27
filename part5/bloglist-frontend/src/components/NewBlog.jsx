531b3c624e8de0a2162aeabad0843981b5f98f8a
Anderson da Silva Marques <anderson.silvamarques00@gmail.com>
2026-06-26 21:56:00 -0300

    blog list frontend step 6

--- part5/bloglist-frontend/src/components/NewBlog.jsx
+++ part5/bloglist-frontend/src/components/NewBlog.jsx
@@ -13,35 +13,34 @@ const NewBlog = ({ title, setTitle, author, setAuthor, url, setUrl, likes, setLi
 		onChange={(e) => setTitle(e.target.value)}
 	    />
 
-	<Input
-	textLabel="author: "
-	type="text"
-	placehd="type an author"
-	value={author}
-	onChange={(e) => setAuthor(e.target.value)}
-	/>
+	    <Input
+		textLabel="author: "
+		type="text"
+		placehd="type an author"
+		value={author}
+		onChange={(e) => setAuthor(e.target.value)}
+	    />
 
-	<Input 
-	textLabel="url: "
-	type="text"
-	placehd="type an url"
-	value={url}
-	onChange={(e) => setUrl(e.target.value)}
-	/>
+	    <Input 
+		textLabel="url: "
+		type="text"
+		placehd="type an url"
+		value={url}
+		onChange={(e) => setUrl(e.target.value)}
+	    />
 
-	<Input
-	textLabel="Likes: "
-	type="number"
-	placehd="type the book's likes' number"
-	value={likes}
-	onChange={(e) => setLikes(e.target.value)}
+	    <Input
+		textLabel="Likes: "
+		type="number"
+		placehd="type the book's likes' number"
+		value={likes}
+		onChange={(e) => setLikes(e.target.value)}
+	    />
 
-	/>
-
-	<Button
-	buttonText="create"
-	onClick={() => createNewBook(title, author, url, likes)}
-	/>
+	    <Button
+		buttonText="create"
+		onClick={() => createNewBook(title, author, url, likes)}
+	    />
 	</div>
     )
 
