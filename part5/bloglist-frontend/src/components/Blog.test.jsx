ccababc9a4ee9b04eba8c2304deeff2aace56993
Anderson da Silva Marques <anderson.silvamarques00@gmail.com>
2026-07-10 23:01:51 -0300

    blog list tests step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog.jsx'
 import '../App.css'
+import { expect } from 'vitest'
 
-test('render blog\'s title and author', () => {
+test('render blog\'s title and author, but not render url or number of likes by default', () => {
     
     const user = { username: "anderson" }
     const setUser = () => {}
@@ -56,5 +58,62 @@ test('render blog\'s title and author', () => {
     expect(authorTest).toBeDefined()
     expect(urlTest).toBeNull()
     expect(likesTest).toBeNull()
+
+})
+
+test('clicking the view\'s button shows the url and likes', async () => {
+    
+    const user = { username: "anderson" }
+    const setUser = () => {}
+    const setBlogs = () => {}
+    const title = ""
+    const setTitle = () => {}
+    const author = ""
+    const setAuthor = () => {}
+    const url = ""
+    const setUrl = () => {}
+    const likes = 0 
+    const setLikes = () => {}
+    const message = ""
+    const setMessage = () => {}
+    const css = ""
+    const setCss = () => {}
+    
+    const blogs = [
+	{
+	    title: "Os sete hábitos de pessoas altamente eficazes",
+	    author: "Stephen R. Covey",
+	    url: "https://www.pessoasaltamenteeficazes.com",
+	    likes: 100,
+	    user: {
+		username: "anderson",
+		name: "Anderson",
+		id: "6a170b8378ecddea095aeb3e",
+	    },
+	    id: "6a36b282b3bbd7678912efa9"
+	}
+    ]
+
+    render(
+	<Blog
+	    blogs={blogs} setBlogs={setBlogs}
+	    user={user} setUser={setUser}
+	    title={title} setTitle={setTitle}
+	    author={author} setAuthor={setAuthor}
+	    url={url} setUrl={setUrl}
+	    likes={likes} setLikes={setLikes}
+	    message={message} setMessage={setMessage}
+	    css={css} setCss={setCss}
+	    
+	/>
+    )
+    
+    const urlTest = screen.getByText('https://www.pessoasaltamenteeficazes.com', { exact: false})
+    const likesTest = screen.getByText('100', { exact: false})
+    const userMock = userEvent.setup()
+    const button = screen.getByRole('button', { name: /view/i })
+    await userMock.click(button)
+    expect(urlTest).toBeDefined()
+    expect(likesTest).toBeDefined()
 
 })
